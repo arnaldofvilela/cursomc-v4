ab1a378636a825449bd96722c85e271e06395003
Arnaldo Neto <vilela-neto@hotmail.com>
2022-10-06 16:43:54 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/arnaldoneto/cursomc/domain/Cliente.java
+++ src/main/java/com/arnaldoneto/cursomc/domain/Cliente.java
@@ -1,6 +1,7 @@
 package com.arnaldoneto.cursomc.domain;
 
 import com.arnaldoneto.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Cliente implements Serializable {
     private String email;
     private String cpfOuCnpj;
     private Integer tipo;
+    @JsonManagedReference
     @OneToMany(mappedBy="cliente")
     private List<Endereco> enderecos = new ArrayList<>();
 
